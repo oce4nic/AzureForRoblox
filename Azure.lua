f933197310539fcb7850d9835646bb98a922da26
vlctyy <kharispowell@hotmail.com>
2024-08-26 11:38:48 -0500

    Update Azure.lua

--- Azure.lua
+++ Azure.lua
@@ -6,7 +6,7 @@ local Window = Rayfield:CreateWindow({
     LoadingSubtitle = "By RontuYT on YouTube.",
     ConfigurationSaving = {
         Enabled = true,
-        FolderName = "AzureConfigs", -- Change if you want to store configs in a different folder
+        FolderName = "AzureConfigs", -- Change if i need to store configs in a different folder this hopefully just works as a demo for the realy thing.
         FileName = "AzureSettings"
     },
     KeySystem = false,
@@ -69,15 +69,27 @@ SettingsSection:CreateDropdown({
     end
 })
 
--- Button for FFlagDebugGreySky
-SettingsSection:CreateButton({
+-- Placeholder for game IDs
+local GameIDs = MainTab:CreateSection("Game IDs")
+GameIDs:CreateLabel("Game Lobby ID: [Enter ID Here]")
+GameIDs:CreateLabel("Game Mega ID: [Enter ID Here]")
+GameIDs:CreateLabel("Game Micro ID: [Enter ID Here]")
+
+-- Custom Client tab setup
+local CustomClientTab = Window:CreateTab("CustomClient", 4483362458) -- Tab name and optional icon ID ig
+
+-- Section for Custom Client features
+local CustomClientSection = CustomClientTab:CreateSection("Custom Client Features")
+
+-- Button for FFlagDebugGreySky in CustomClient
+CustomClientSection:CreateButton({
     Name = "FFlagDebugGreySky",
     Callback = function()
         local greySky = Instance.new("Sky")
         greySky.Name = "GreySky"
-        greySky.SkyboxBk = "rbxassetid://7036680743" -- A placeholder skybox ID with grey tones
+        greySky.SkyboxBk = "rbxassetid://7036680743" -- A placeholder skybox ID with grey elememnts reminder to chnage below
         greySky.SkyboxDn = ""
-        greySky.SkyboxFt = "rbxassetid://7036680743" -- Apply similar skybox ID to all directions for consistency
+        greySky.SkyboxFt = "rbxassetid://7036680743" -- I need to change these later
         greySky.SkyboxLf = "rbxassetid://7036680743"
         greySky.SkyboxRt = "rbxassetid://7036680743"
         greySky.SkyboxUp = "rbxassetid://7036680743"
@@ -87,11 +99,25 @@ SettingsSection:CreateButton({
     end
 })
 
--- Placeholder for game IDs
-local GameIDs = MainTab:CreateSection("Game IDs")
-GameIDs:CreateLabel("Game Lobby ID: [Enter ID Here]")
-GameIDs:CreateLabel("Game Mega ID: [Enter ID Here]")
-GameIDs:CreateLabel("Game Micro ID: [Enter ID Here]")
+-- Dropdown for selecting custom fonts
+local fonts = {"Legacy", "Arial", "SourceSans", "ComicSans", "Gotham", "GothamBold", "GothamBlack"}
+
+CustomClientSection:CreateDropdown({
+    Name = "Select Custom Font",
+    Options = fonts,
+    CurrentOption = "SourceSans",
+    Flag = "CustomFontDropdown",
+    Callback = function(selectedFont)
+        -- Change the font of all GUI elements in the game
+        for _, obj in pairs(game:GetDescendants()) do
+            if obj:IsA("TextLabel") or obj:IsA("TextButton") or obj:IsA("TextBox") then
+                obj.Font = Enum.Font[selectedFont]
+            end
+        end
+        
+        print("Custom Font Applied: " .. selectedFont)
+    end
+})
 
 -- Load the configuration
 Rayfield:LoadConfiguration()
